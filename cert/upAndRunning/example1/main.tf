437280b06bee1039b1c440012043d2bbe3a7ae1d
Luis Alejandro <luis@Luiss-MacBook-Air.local>
2021-02-02 13:24:19 -0500

    variables and new commands

--- cert/upAndRunning/example1/main.tf
+++ cert/upAndRunning/example1/main.tf
@@ -3,10 +3,44 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance_sg" {
+  name        = "instance-sg"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+
 }
---
+++ cert/upAndRunning/example1/variables.tf
@@ -0,0 +1,50 @@
+variable "number_example" {
+  description = "An example of a number"
+  type        = number
+  default     = 42
+}
+
+variable "list_example" {
+  description = "example of list in terra"
+  type        = list
+  default     = ["a", "b", "c"]
+}
+
+variable "list_numeric_example" {
+  description = "list of numbers"
+  type        = list(number)
+  default     = [1, 2, 3]
+}
+
+variable "map_example" {
+  description = "an example of map in terra"
+  type        = map(string)
+  default = {
+    key1 = "value 1"
+    key2 = "value 2"
+    key3 = "value 3"
+  }
+
+}
+
+variable "object_example" {
+  description = "Example of structural type in terra"
+  type = object({
+    name   = string
+    age    = number
+    tags   = list(string)
+    enable = bool
+  })
+
+  default = {
+    name   = "value 1"
+    age    = 34
+    tags   = ["a", "b", "c"]
+    enable = true
+  }
+}
+
+variable "server_port" {
+  description = "port to get all  http request"
+  type = number
+}
